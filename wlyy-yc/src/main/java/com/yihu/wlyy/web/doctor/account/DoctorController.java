ca5ccbc627889a2894dbf1e7dba6771ead11129c
esb <494679975@qq.com>
2016-09-01 09:02:32 +0800

    代码提交

--- wlyy-yc/src/main/java/com/yihu/wlyy/web/doctor/account/DoctorController.java
+++ wlyy-yc/src/main/java/com/yihu/wlyy/web/doctor/account/DoctorController.java
@@ -17,27 +17,6 @@ public class DoctorController extends BaseController {
 
 
     /**
-     * 社区医院下医生列表查询接口 没分页
-     *
-     * @param hospital 医院标识
-     * @return
-     */
-    @RequestMapping(value = "getDoctorsByhospitalNoPage")
-    @ResponseBody
-    public String getDoctorsByhospitalNoPage(
-            @RequestParam(required = false) String hospital,
-            @RequestParam(required = false) String name,
-            @RequestParam(required = false) Integer type) {
-        try {
-
-            return write(200, "获取医院医生列表成功！", "list", "");
-        } catch (Exception e) {
-            error(e);
-            return error(-1, "获取医院医生列表失败！");
-        }
-    }
-
-    /**
      * 社区医院下医生列表查询接口 有分页
      *
      * @param hospital 医院标识
@@ -58,68 +37,6 @@ public class DoctorController extends BaseController {
         }
     }
 
-    /**
-     * 社区医院列表查询接口
-     *
-     * @param type     医院类型：0全部，1大医院，2社区医院
-     * @param province 患标识
-     * @param city     城市标识
-     * @param key      搜索关键字，用于搜索医院名称
-     * @return
-     */
-    @RequestMapping(value = "hospitals")
-    @ResponseBody
-    public String hospitals(
-            int type,
-            @RequestParam(required = false) String province,
-            @RequestParam(required = false) String city,
-            @RequestParam(required = false) String town,
-            @RequestParam(required = false) String key,
-            long id,
-            int pagesize) {
-        try {
-            return write(200, "获取医院列表成功！", "list", "");
-        } catch (Exception e) {
-            error(e);
-            return error(-1, "获取医院列表失败！");
-        }
-    }
-
-
-    /**
-     * 修改密码
-     *
-     * @param oldpwd 旧密码
-     * @param newpwd 新密码
-     * @return
-     */
-    @RequestMapping(value = "updatepwd")
-    @ResponseBody
-    public String updatepwd(String oldpwd, String newpwd) {
-        try {
-            return "";
-        } catch (Exception e) {
-            error(e);
-            return invalidUserException(e, -1, "修改失败！");
-        }
-    }
-
-    /**
-     * 退出登录
-     *
-     * @return
-     */
-    @RequestMapping(value = "loginout")
-    @ResponseBody
-    public String loginout() {
-        try {
-
-            return success("已成功退出！");
-        } catch (Exception e) {
-            error(e);
-            return invalidUserException(e, -1, "操作失败！");
-        }
-    }
 
     /**
      * 医生基本信息查询接口
@@ -128,102 +45,12 @@ public class DoctorController extends BaseController {
      */
     @RequestMapping(value = "baseinfo")
     @ResponseBody
-    public String baseinfo() {
+    public String baseinfo(String doctor) {
         try {
             return "";
         } catch (Exception e) {
             error(e);
             return invalidUserException(e, -1, "医生信息查询失败！");
-        }
-    }
-
-    /**
-     * 医生基本信息查询接口
-     *
-     * @return
-     */
-    @RequestMapping(value = "baseinfoCount")
-    @ResponseBody
-    public String baseinfoCount(
-            String doctorType) {
-        try {
-            return "";
-        } catch (Exception e) {
-            error(e);
-            return invalidUserException(e, -1, "医生信息查询失败！");
-        }
-    }
-
-    /**
-     * 患者基本信息保存
-     *
-     * @param photo     頭像
-     * @param sex       性別
-     * @param expertise 专长
-     * @param introduce 简介
-     * @param province  省标识
-     * @param city      市标识
-     * @return
-     */
-    @RequestMapping(value = "save")
-    @ResponseBody
-    public String save(
-            @RequestParam(required = false) String photo,
-            @RequestParam(required = false) Integer sex,
-            @RequestParam(required = false) String expertise,
-            @RequestParam(required = false) String introduce,
-            @RequestParam(required = false) String province,
-            @RequestParam(required = false) String city,
-            @RequestParam(required = false) String mobile) {
-        try {
-
-            return null;
-        } catch (Exception e) {
-            error(e);
-            return invalidUserException(e, -1, "保存失败！");
-        }
-    }
-
-    /**
-     * 红黄绿标患者数查询接口
-     *
-     * @return
-     */
-    @RequestMapping(value = "patients")
-    @ResponseBody
-    public String patients() {
-        try {
-            return write(200, "查询成功！", "data", "");
-        } catch (Exception e) {
-            error(e);
-            return error(-1, "查询失败！");
-        }
-    }
-
-    /**
-     * 邀请医生咨询
-     *
-     * @param patientCode
-     * @param page
-     * @param pageSize
-     * @return
-     */
-    @RequestMapping(value = "/hospital_list_type")
-    @ResponseBody
-    public String getHospitalList(
-            @RequestParam(required = true) int doctorType,
-            @RequestParam(required = true) int consultType,
-            @RequestParam(required = true) String patientCode,
-            @RequestParam(required = false) String name,
-            @RequestParam(required = true) Integer page,
-            @RequestParam(required = true) Integer pageSize) {
-        try {
-            return write(200, "查询成功！", "list", "");
-
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            error(ex);
-            return error(-1, "查询失败！");
         }
     }
 
--- wlyy-yc/src/main/java/com/yihu/wlyy/web/doctor/sign/DoctorFamilyContractController.java
+++ wlyy-yc/src/main/java/com/yihu/wlyy/web/doctor/sign/DoctorFamilyContractController.java
@@ -1,7 +1,15 @@
 package com.yihu.wlyy.web.doctor.sign;
 
+import com.yihu.wlyy.web.BaseController;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.net.URLDecoder;
+import java.util.Date;
 
 /**
  * 医生端：家庭签约控制类
@@ -10,8 +18,106 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 @RequestMapping(value = "/doctor/family_contract")
-public class DoctorFamilyContractController  {
+public class DoctorFamilyContractController extends BaseController {
 
+    /**
+     * 患者签约申请处理接口
+     *
+     * @param signType         是否有三师签约 1 否 2是
+     * @param healthDoctor     健康管理师标识
+     * @param healthDoctorName 健康管理师姓名
+     * @param majorDoctor      专科医生标识
+     * @param disease          疾病code
+     * @param majorDoctorName 专科医生姓名
+     * @param majorDoctor 专科医生code
+     * @param msgid            消息id
+     * @param patientIDcard    患者身份证
+     * @param type             处理类型：1同意，2拒绝
+     * @param group
+     * @return
+     */
+    @RequestMapping(value = "sign")
+    @ResponseBody
+    public String sign(
+            @RequestParam(required = true, defaultValue = "1") String signType,
+            String healthDoctor,
+            @RequestParam(required = false, defaultValue = "1") String disease,
+            String healthDoctorName,
+            @RequestParam(required = false) String majorDoctor,
+            @RequestParam(required = false) String majorDoctorName,
+            long msgid,
+            String patientIDcard,
+            int type,String group,
+            @RequestParam(required = false,defaultValue = "0") String expenses) {
+        try {
+            return "";
+        } catch (Exception e) {
+            error(e);
+            return error(-1, "操作失败！");
+        }
+    }
 
+    /**
+     * 患者解约申请处理接口
+     *
+     * @param msgid   消息id
+     * @param patient 患者标识
+     * @param type    处理类型：1同意，2拒绝
+     * @param reason  解约原因
+     * @return
+     */
+    @RequestMapping(value = "surrender")
+    @ResponseBody
+    public String surrender(
+            long msgid,
+            String patient,
+            int type,
+            @RequestParam(required = false) String reason) {
+        try {
 
+            return error(-1, "操作失败！");
+        } catch (Exception e) {
+            error(e);
+            return error(-1, "操作失败！");
+        }
+    }
+    /**
+     * 获取签约患者信息
+     * @param status 签约状态 1:待签约 2, 待解约 3 已签约,4已经解约
+     * @param doctorType 医生类别 健管 3  全科 2
+     * @return
+     */
+    @RequestMapping("/sign_info")
+    public String getSignInfoByDoctor(
+            int status,
+            int doctorType,
+            int page,
+            int pageSize) {
+        try{
+            return write(200, "查询成功！", "data", "");
+        }
+        catch(Exception ex)
+        {
+            ex.printStackTrace();
+            return error(-1,"系统错误,请联系管理员!");
+        }
+    }
+
+    /**
+     * 校验是否有签约信息
+     *
+     * @param idcard
+     * @return
+     */
+    @RequestMapping(value = "check")
+    @ResponseBody
+    public String checkSign(String idcard) {
+        try {
+
+            return write(200, "签约验证成功！", "data", "");
+        } catch (Exception e) {
+            error(e);
+            return error(-1, "签约验证异常！");
+        }
+    }
 }

be0a563efe298634c65cc8932162003dd85d9e67
Airhead <L4qiang@gmail.com>
2016-09-04 09:36:55 +0800

    增加用户登录逻辑

---
+++ wlyy-yc/src/main/java/com/yihu/wlyy/services/user/UserSessionService.java
@@ -0,0 +1,38 @@
+package com.yihu.wlyy.services.user;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.yihu.wlyy.models.user.UserDao;
+import com.yihu.wlyy.models.user.UserSessionDao;
+import com.yihu.wlyy.models.user.UserSessionModel;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @created Airhead 2016/9/4.
+ */
+public class UserSessionService {
+    @Autowired
+    private UserSessionDao userSessionDao;
+    @Autowired
+    private UserDao userDao;
+
+    public String loginInfo(String userCode) {
+        UserSessionModel userSessionModel = userSessionDao.findOne(userCode);
+        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectNode objectNode = objectMapper.createObjectNode();
+        if (userSessionModel == null){
+
+        }
+
+        return null;
+    }
+
+    public String login(String userName, String password) {
+
+        return null;
+    }
+
+    public String loginCallback() {
+        return null;
+    }
+}

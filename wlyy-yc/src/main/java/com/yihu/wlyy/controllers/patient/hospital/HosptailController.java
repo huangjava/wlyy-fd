8573a3cc18663ed6ab2f9ae4fc92fdaeda22e36a
demon <huangzy0228@163.com>
2016-09-04 14:40:10 +0800

    患者端-接口swagger添加

--- wlyy-yc/src/main/java/com/yihu/wlyy/controllers/patient/hospital/HosptailController.java
+++ wlyy-yc/src/main/java/com/yihu/wlyy/controllers/patient/hospital/HosptailController.java
@@ -1,8 +1,11 @@
 package com.yihu.wlyy.controllers.patient.hospital;
 
 import com.yihu.wlyy.controllers.BaseController;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -16,9 +19,12 @@ public class HosptailController extends BaseController {
      * 根据市得到区
      * @return
      */
-    @RequestMapping(value = "getTownByCityCode")
+    @RequestMapping(value = "getTownByCityCode", produces = "application/json;charset=UTF-8", method = RequestMethod.GET)
     @ResponseBody
-    public String getTownByCityCode(String city) {
+    @ApiOperation(value = "根据市得到区", produces = "application/json", notes = "根据市得到区")
+    public String getTownByCityCode(
+            @ApiParam(name = "city", value = "城市Code", required = true)
+            String city) {
         try {
             return write(200, "查询成功", "list", "");
         } catch (Exception e) {
@@ -32,9 +38,12 @@ public class HosptailController extends BaseController {
      * @param town
      * @return
      */
-    @RequestMapping(value = "getHositalByTownCode")
+    @RequestMapping(value = "getHositalByTownCode", produces = "application/json;charset=UTF-8", method = RequestMethod.GET)
     @ResponseBody
-    public String getHositalByTownCode(String town) {
+    @ApiOperation(value = "根据区得到机构", produces = "application/json", notes = "根据区得到机构")
+    public String getHositalByTownCode(
+            @ApiParam(name = "town", value = "区", required = true)
+            String town) {
         try {
             return write(200, "查询成功", "list", "");
         } catch (Exception e) {
@@ -51,13 +60,18 @@ public class HosptailController extends BaseController {
      * @param pageSize 页数
      * @return
      */
-    @RequestMapping(value = "/hospital_list")
+    @RequestMapping(value = "/hospital_list", produces = "application/json;charset=UTF-8", method = RequestMethod.GET)
     @ResponseBody
+    @ApiOperation(value = "根据类别获取医院列表", produces = "application/json", notes = "根据类别获取医院列表")
     public String getHospitalList(
-            @RequestParam(required = true) Integer type,
-            @RequestParam(required = false) String query,
-            @RequestParam(required = true) long id,
-            @RequestParam(required = true) Integer pageSize) {
+            @ApiParam(name = "type", value = "医院类型", required = false)
+            @RequestParam(required = false) Integer type,
+            @ApiParam(name = "query", value = "医院名称", required = true)
+            @RequestParam(required = true) String query,
+            @ApiParam(name = "id", value = "主键", required = false)
+            @RequestParam(required = false) long id,
+            @ApiParam(name = "pageSize", value = "每页大小", required = false)
+            @RequestParam(required = false) Integer pageSize) {
         try {
 
             return write(200, "查询成功！", "list", "");
@@ -75,12 +89,17 @@ public class HosptailController extends BaseController {
      * @param pageSize 页数
      * @return
      */
-    @RequestMapping(value = "/doctor_list")
+    @RequestMapping(value = "/doctor_list", produces = "application/json;charset=UTF-8", method = RequestMethod.GET)
     @ResponseBody
+    @ApiOperation(value = "根据医院标识获取医生信息", produces = "application/json", notes = "根据医院标识获取医生信息")
     public String getDoctorByHospital(
+            @ApiParam(name = "hospital", value = "医院标识", required = false)
             @RequestParam(required = false) String hospital,
+            @ApiParam(name = "query", value = "查询条件 ：医生名称", required = false)
             @RequestParam(required = false) String query,
+            @ApiParam(name = "id", value = "主键", required = true)
             @RequestParam(required = true) long id,
+            @ApiParam(name = "pageSize", value = "页数", required = true)
             @RequestParam(required = true) Integer pageSize) {
 
         try {
--- wlyy-yc/src/main/java/com/yihu/wlyy/controllers/patient/sign/FamilyContractController.java
+++ wlyy-yc/src/main/java/com/yihu/wlyy/controllers/patient/sign/FamilyContractController.java
@@ -1,8 +1,11 @@
 package com.yihu.wlyy.controllers.patient.sign;
 
 import com.yihu.wlyy.controllers.BaseController;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -20,10 +23,13 @@ public class FamilyContractController extends BaseController {
      * @param doctor
      * @return
      */
-    @RequestMapping(value = "unsign")
+    @RequestMapping(value = "unsign", produces = "application/json;charset=UTF-8", method = RequestMethod.POST)
     @ResponseBody
+    @ApiOperation(value = "取消签约申请", produces = "application/json", notes = "取消签约申请")
     public String unsign(
+            @ApiParam(name = "doctor", value = "医生Code", required = true)
             String doctor,
+            @ApiParam(name = "patientCode", value = "患者Code", required = false)
             @RequestParam(required = false) String patientCode
     ) {
         try {
@@ -41,10 +47,18 @@ public class FamilyContractController extends BaseController {
      * @param doctorName 医生姓名
      * @return
      */
-    @RequestMapping(value = "surrender")
+    @RequestMapping(value = "surrender", produces = "application/json;charset=UTF-8", method = RequestMethod.POST)
     @ResponseBody
-    public String surrender(String doctor, String doctorName, String reason,
-                            @RequestParam(required = false) String patientCode) {
+    @ApiOperation(value = "申请解约", produces = "application/json", notes = "申请解约")
+    public String surrender(
+            @ApiParam(name = "doctor", value = "医生Code", required = true)
+            String doctor,
+            @ApiParam(name = "doctorName", value = "医生名称", required = true)
+            String doctorName,
+            @ApiParam(name = "reason", value = "理由", required = true)
+            String reason,
+            @ApiParam(name = "patientCode", value = "患者Code", required = true)
+            @RequestParam(required = true) String patientCode) {
         try {
 
             return success("解约申请已发送！");
@@ -59,9 +73,12 @@ public class FamilyContractController extends BaseController {
      * @param patientCode 患者标识
      * @return
      */
-    @RequestMapping(value = "getSignMessage")
+    @RequestMapping(value = "getSignMessage", produces = "application/json;charset=UTF-8", method = RequestMethod.GET)
     @ResponseBody
-    public String getSignDoctorMessage(String patientCode){
+    @ApiOperation(value = "得到患者的签约的医生的信息", produces = "application/json", notes = "得到患者的签约的医生的信息")
+    public String getSignDoctorMessage(
+            @ApiParam(name = "patientCode", value = "患者Code", required = true)
+            String patientCode){
         try {
 
 
@@ -75,37 +92,54 @@ public class FamilyContractController extends BaseController {
 
     /**
      * 发送签约申请
-     * @param province 省代码
-     * @param provinceName 省名称
-     * @param city 城市代码
-     * @param cityName 城市名称
-     * @param town 区县代码
-     * @param townName 区县名称
+     //     * @param province 省代码
+     //     * @param provinceName 省名称
+     //     * @param city 城市代码
+     //     * @param cityName 城市名称
+     //     * @param town 区县代码
+     //     * @param townName 区县名称
      * @param address 详细地址
-     * @param name 患者姓名
-     * @param doctor 医生标识
+    //     * @param name 患者姓名
+    //     * @param doctor 医生标识
      * @param idcard 患者身份证号
      * @param ssc 患者社保卡号
      * @param mobile 患者手机号
      * @param emerMobile 患者紧急联系人
+     * @param teamCode 团队唯一码
+     * @param teamName 团队名称
      */
-    @RequestMapping(value = "sign")
+    @RequestMapping(value = "sign", produces = "application/json;charset=UTF-8", method = RequestMethod.POST)
     @ResponseBody
+    @ApiOperation(value = "发送签约申请", produces = "application/json", notes = "发送签约申请")
     public String sign(
-            @RequestParam(required = false) String province,
-            @RequestParam(required = false) String provinceName,
-            @RequestParam(required = false) String city,
-            @RequestParam(required = false) String cityName,
-            @RequestParam(required = false) String town,
-            @RequestParam(required = false) String townName,
+//            @RequestParam(required = false) String province,
+//            @RequestParam(required = false) String provinceName,
+//            @RequestParam(required = false) String city,
+//            @RequestParam(required = false) String cityName,
+//            @RequestParam(required = false) String town,
+//            @RequestParam(required = false) String townName,
+//            String name,
+//            String doctor,
+//            String doctorName,
+            @ApiParam(name = "orgCode", value = "机构Code", required = true)
+            @RequestParam(required = true) String orgCode,
+            @ApiParam(name = "teamCode", value = "团队Code", required = true)
+            @RequestParam(required = true) String teamCode,
+            @ApiParam(name = "address", value = "详细地址", required = false)
             @RequestParam(required = false) String address,
-            String name,
-            String doctor,
-            String doctorName,
+            @ApiParam(name = "patientCode", value = "患者Code", required = true)
             @RequestParam(required = false) String patientCode,
+            @ApiParam(name = "idcard", value = "患者身份证号", required = true)
             @RequestParam(required = false)String idcard,
+            @ApiParam(name = "orgName", value = "机构名称", required = true)
+            @RequestParam(required = false)String orgName,
+            @ApiParam(name = "teamName", value = "团队名称", required = true)
+            @RequestParam(required = false)String teamName,
+            @ApiParam(name = "ssc", value = "医保卡号", required = true)
             String ssc,
+            @ApiParam(name = "mobile", value = "手机号", required = true)
             @RequestParam(required = false) String mobile,
+            @ApiParam(name = "emerMobile", value = "患者紧急联系人", required = true)
             @RequestParam(required = false) String emerMobile) {
         try {
 

0353fe52316b5f76ab1aec240ae25c1bb0d296f9
hill9868 <hill9868@163.com>
2016-09-12 22:00:35 +0800

    xml工具类引入

---
+++ wlyy-yc/src/main/java/com/yihu/wlyy/controllers/user/LoginAppController.java
@@ -0,0 +1,13 @@
+package com.yihu.wlyy.controllers.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @created Airhead 2016/9/4.
+ */
+@Controller
+@RequestMapping(value = "/loginApp")
+public class LoginAppController {
+
+}

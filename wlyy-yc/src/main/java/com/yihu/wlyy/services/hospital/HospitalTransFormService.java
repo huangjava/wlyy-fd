8dd733a4b83afd03873f86db824dd1c20a8a04a6
demon <huangzy0228@163.com>
2016-09-12 11:27:02 +0800

    xml转map工具类 修改添加

--- wlyy-yc/src/main/java/com/yihu/wlyy/services/hospital/HospitalTransFormService.java
+++ wlyy-yc/src/main/java/com/yihu/wlyy/services/hospital/HospitalTransFormService.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yihu.wlyy.util.XMLUtil;
 import org.dom4j.DocumentException;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -17,7 +17,7 @@ import java.util.Map;
  * @vsrsion 1.0
  * Created at 2016/9/10.
  */
-@Service
+@Component
 public class HospitalTransFormService {
 
     /**
@@ -46,8 +46,8 @@ public class HospitalTransFormService {
             if (teams!=null && teams.size()>0){
                 for (Map<String,Object> team:teams){
                     Map<String,Object> obj = new HashMap<String,Object>();
-                    obj.put("name",team.get("TEAMNAME").toString());
-                    obj.put("code",team.get("TEAMID").toString());
+                    obj.put("name", team.get("TEAMNAME"));
+                    obj.put("code", team.get("TEAMID"));
                     result.add(obj);
                 }
             }
@@ -63,11 +63,55 @@ public class HospitalTransFormService {
      * @param teamCode 团队主键
      * @return
      */
-    public Map<String,Object> getTeamInfoByTeamCode(String teamCode){
-        Map<String,Object> map = new HashMap<>();
+    public static Map<String,Object> getTeamInfoByTeamCode(String teamCode){
+        Map<String,Object> result = new HashMap<>();
+        //TODO 调用东软接口返回数据
+        String responseXml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "<MSGFORM>\n" +
+                "  <XMLDATA>\n" +
+                "    <TEAMID>74529931-1445-468d-8873-abfa63734e7c</TEAMID>\n" +
+                "    <TEAMNAME>高血压团队</TEAMNAME>\n" +
+                "    <ORGCODE>123456</ORGCODE>\n" +
+                "    <CREATEDUNITNAME>宜昌市惠民医院</CREATEDUNITNAME>\n" +
+                "    <CREATEDUNITCODE>420503003000</CREATEDUNITCODE>\n" +
+                "    <CREATEDTIME>2016-08-10</CREATEDTIME>\n" +
+                "    <TEAMDESC>团队简介</TEAMDESC>\n" +
+                "    <USERID>42589bc0-e069-4a92-a70c-c8a19be9d7d2</USERID>\n" +
+                "    <USER_FULLNAME>宋文</USER_FULLNAME>\n" +
+                "    <DEPT_NAME>儿科</DEPT_NAME>\n" +
+                "  </XMLDATA>\n" +
+                "  <XMLDATA>\n" +
+                "    <TEAMID>74529931-1445-468d-8873-abfa63734e7c</TEAMID>\n" +
+                "    <TEAMNAME>高血压团队</TEAMNAME>\n" +
+                "    <ORGCODE>123456</ORGCODE>\n" +
+                "    <CREATEDUNITNAME>宜昌市惠民医院</CREATEDUNITNAME>\n" +
+                "    <CREATEDUNITCODE>420503003000</CREATEDUNITCODE>\n" +
+                "    <CREATEDTIME>2016-08-10</CREATEDTIME>\n" +
+                "    <TEAMDESC>团队简介</TEAMDESC>\n" +
+                "    <USERID>42589bc0-e069-4a92-a70c-c8a19be9d333</USERID>\n" +
+                "    <USER_FULLNAME>王明</USER_FULLNAME>\n" +
+                "    <DEPT_NAME>儿科</DEPT_NAME>\n" +
+                "  </XMLDATA>\n" +
+                "</MSGFORM>\n";
 
+        try {
+            Map<String,Object> map  = XMLUtil.xml2map(responseXml);
+            List<Map<String,Object>> teams = (List<Map<String, Object>>) map.get("XMLDATA");
+            if (teams!=null && teams.size()>0){
+                Map<String,Object> obj = teams.get(0);
+                result.put("teamCode",obj.get("TEAMID"));
+                result.put("teamName",obj.get("TEAMNAME"));
+                result.put("orgCode",obj.get("ORGCODE"));
+                result.put("orgName",obj.get("TEAMNAME"));//toset
+                result.put("introduce",obj.get("TEAMDESC"));
+                result.put("photo",obj.get("TEAMNAME"));//toset
 
-        return map;
+            }
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        }
+
+        return result;
     }
 
     /**
@@ -76,10 +120,28 @@ public class HospitalTransFormService {
      * @return
      */
     public Map<String,Object> getDoctorInfo(String doctorCode){
-        Map<String,Object> map = new HashMap<>();
+        Map<String,Object> result = new HashMap<>();
+        //TODO 调用东软接口返回数据
+        String responseXml = "";
 
+        try {
+            Map<String,Object> map  = XMLUtil.xml2map(responseXml);
+            List<Map<String,Object>> teams = (List<Map<String, Object>>) map.get("XMLDATA");
+            if (teams!=null && teams.size()>0){
+                Map<String,Object> obj = teams.get(0);
+//                result.put("code",obj.get("TEAMID"));
+                result.put("name",obj.get("USER_FULLNAME"));
+                result.put("introduce",obj.get("UNIT_NAME"));
+                result.put("hospital_name",obj.get("UNIT_NAME"));//toset
+                result.put("expertise",obj.get("SPECIALTY"));
+                result.put("photo",obj.get("PHOTO"));//toset
 
-        return map;
+            }
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        }
+
+        return result;
     }
 
     /**
@@ -124,7 +186,8 @@ public class HospitalTransFormService {
     }
 
     public static void main(String[] args) {
-        List<Map<String,Object> > map = getTeamsByorgCode("");
+//        List<Map<String,Object> > list = getTeamsByorgCode("");
+        Map<String ,Object> map = getTeamInfoByTeamCode("");
         ObjectMapper objectMapper = new ObjectMapper();
         try {
             String json = objectMapper.writeValueAsString(map);
--- wlyy-yc/src/main/java/com/yihu/wlyy/util/XMLUtil.java
+++ wlyy-yc/src/main/java/com/yihu/wlyy/util/XMLUtil.java
@@ -548,4 +548,100 @@ public class XMLUtil {
             }
         }
     }
+
+
+
+    public static Map xmltoMap(String xml) {
+        try {
+            Map map = new HashMap();
+            Document document = DocumentHelper.parseText(xml);
+            Element nodeElement = document.getRootElement();
+            List node = nodeElement.elements();
+            for (Iterator it = node.iterator(); it.hasNext();) {
+                Element elm = (Element) it.next();
+                map.put(elm.getName(), elm.getText());
+                elm = null;
+            }
+            node = null;
+            nodeElement = null;
+            document = null;
+            return map;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static List xmltoList(String xml) {
+        try {
+            List<Map> list = new ArrayList<Map>();
+            Document document = DocumentHelper.parseText(xml);
+            Element nodesElement = document.getRootElement();
+            List nodes = nodesElement.elements();
+            for (Iterator its = nodes.iterator(); its.hasNext();) {
+                Element nodeElement = (Element) its.next();
+                Map map = xmltoMap(nodeElement.asXML());
+                list.add(map);
+                map = null;
+            }
+            nodes = null;
+            nodesElement = null;
+            document = null;
+            return list;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /* ============================== 以下暂时添加，后面可删除 ========================================= */
+//    public static String maptoXml(Map map) {
+//        Document document = DocumentHelper.createDocument();
+//        Element nodeElement = document.addElement("node");
+//        for (Object obj : map.keySet()) {
+//            Element keyElement = nodeElement.addElement("key");
+//            keyElement.addAttribute("label", String.valueOf(obj));
+//            keyElement.setText(String.valueOf(map.get(obj)));
+//        }
+//        return doc2String(document);
+//    }
+//
+//    public static String listtoXml(List list) throws Exception {
+//        Document document = DocumentHelper.createDocument();
+//        Element nodesElement = document.addElement("nodes");
+//        int i = 0;
+//        for (Object o : list) {
+//            Element nodeElement = nodesElement.addElement("node");
+//            if (o instanceof Map) {
+//                for (Object obj : ((Map) o).keySet()) {
+//                    Element keyElement = nodeElement.addElement("key");
+//                    keyElement.addAttribute("label", String.valueOf(obj));
+//                    keyElement.setText(String.valueOf(((Map) o).get(obj)));
+//                }
+//            } else {
+//                Element keyElement = nodeElement.addElement("key");
+//                keyElement.addAttribute("label", String.valueOf(i));
+//                keyElement.setText(String.valueOf(o));
+//            }
+//            i++;
+//        }
+//        return doc2String(document);
+//    }
+//
+//    public static String doc2String(Document document) {
+//        String s = "";
+//        try {
+//            // 使用输出流来进行转化
+//            ByteArrayOutputStream out = new ByteArrayOutputStream();
+//            // 使用UTF-8编码
+//            OutputFormat format = new OutputFormat("   ", true, "UTF-8");
+//            XMLWriter writer = new XMLWriter(out, format);
+//            writer.write(document);
+//            s = out.toString("UTF-8");
+//        } catch (Exception ex) {
+//            ex.printStackTrace();
+//        }
+//        return s;
+//    }
+
 }

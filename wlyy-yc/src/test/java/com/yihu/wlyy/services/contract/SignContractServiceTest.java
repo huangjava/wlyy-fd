5061f0a02335b70c252105f63810f5140d85c920
Airhead <L4qiang@gmail.com>
2016-09-13 09:02:59 +0800

    修订SignContractService中获取已签约病人列表

---
+++ wlyy-yc/src/test/java/com/yihu/wlyy/services/contract/SignContractServiceTest.java
@@ -0,0 +1,26 @@
+package com.yihu.wlyy.services.contract;
+
+import org.junit.Test;
+
+/**
+ * @created Airhead 2016/9/12.
+ */
+public class SignContractServiceTest {
+
+    private static SignContractService signContractService = new SignContractService();
+    @Test
+    public void getSignedInfoList() throws Exception {
+        String signedInfoList = signContractService.getSignedInfoList("420503003000", "9bf5afea-3200-4489-b93a-b5261351479e", "1", "10");
+        System.out.println(signedInfoList);
+    }
+
+    @Test
+    public void getToSignInfoList() throws Exception {
+
+    }
+
+    @Test
+    public void getNotSignInfoList() throws Exception {
+
+    }
+}
